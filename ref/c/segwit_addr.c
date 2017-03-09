4eaa4523a12997a8caa17805ea47b04056f89ea0
Pieter Wuille <pieter.wuille@gmail.com>
2017-03-09 14:53:58 -0800

    Add C reference

---
+++ ref/c/segwit_addr.c
@@ -0,0 +1,177 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "segwit_addr.h"
+
+uint32_t bech32_polymod_step(uint32_t pre) {
+    int b = pre >> 25;
+    return ((pre & 0x1FFFFFF) << 5) ^
+        (-((b >> 0) & 1) & 0x3b6a57b2UL) ^
+        (-((b >> 1) & 1) & 0x26508e6dUL) ^
+        (-((b >> 2) & 1) & 0x1ea119faUL) ^
+        (-((b >> 3) & 1) & 0x3d4233ddUL) ^
+        (-((b >> 4) & 1) & 0x2a1462b3UL);
+}
+
+static const char* charset = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
+
+static const int8_t charset_rev[128] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+     1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
+};
+
+int bech32_encode(char *output, const char *hrp, const uint8_t *data, size_t data_len) {
+    uint32_t chk = 1;
+    size_t i = 0;
+    while (hrp[i] != 0) {
+        if (!(hrp[i] >> 5)) return 0;
+        chk = bech32_polymod_step(chk) ^ (hrp[i] >> 5);
+        ++i;
+    }
+    if (i + 7 + data_len > 90) return 0;
+    chk = bech32_polymod_step(chk);
+    while (*hrp != 0) {
+        chk = bech32_polymod_step(chk) ^ (*hrp & 0x1f);
+        *(output++) = *(hrp++);
+    }
+    *(output++) = '1';
+    for (i = 0; i < data_len; ++i) {
+        if (*data >> 5) return 0;
+        chk = bech32_polymod_step(chk) ^ (*data);
+        *(output++) = charset[*(data++)];
+    }
+    for (i = 0; i < 6; ++i) {
+        chk = bech32_polymod_step(chk);
+    }
+    chk ^= 1;
+    for (i = 0; i < 6; ++i) {
+        *(output++) = charset[(chk >> ((5 - i) * 5)) & 0x1f];
+    }
+    *output = 0;
+    return 1;
+}
+
+int32_t bech32_decode_fault(size_t *hrp_len, uint8_t *data, size_t *data_len, const char *input) {
+    uint32_t chk = 1;
+    size_t i;
+    size_t input_len = strlen(input);
+    if (input_len < 8 || input[input_len] != 0) {
+        return -1;
+    }
+    *data_len = 0;
+    while (*data_len < input_len && input[input_len - 1 - *data_len] != '1') {
+        ++(*data_len);
+    }
+    *hrp_len = input_len - *data_len - 1;
+    if (*hrp_len < 1 || *data_len < 6) {
+        return -2;
+    }
+    *(data_len) -= 6;
+    for (i = 0; i < *hrp_len; ++i) {
+        if (input[i] < 32 || input[i] > 127) {
+            return -3;
+        }
+        chk = bech32_polymod_step(chk) ^ (input[i] >> 5);
+    }
+    chk = bech32_polymod_step(chk);
+    for (i = 0; i < *hrp_len; ++i) {
+        chk = bech32_polymod_step(chk) ^ (input[i] & 0x1f);
+    }
+    ++i;
+    while (i < input_len) {
+        int v = input[i] & 0x80 ? -2 : charset_rev[(int)input[i]];
+        if (v == -1) {
+            return -4;
+        }
+        if (v == -2) {
+            return -5;
+        }
+        chk = bech32_polymod_step(chk) ^ v;
+        if (i + 6 < input_len) {
+            data[i - *hrp_len - 1] = v;
+        }
+        ++i;
+    }
+    return chk ^ 1;
+}
+
+int bech32_decode(size_t *hrp_len, uint8_t *data, size_t *data_len, const char *input) {
+    return bech32_decode_fault(hrp_len, data, data_len, input) == 0;
+}
+
+static int convert_bits(uint8_t* out, size_t* outlen, int outbits, const uint8_t* in, size_t inlen, int inbits, int pad) {
+    uint32_t val = 0;
+    int bits = 0;
+    uint32_t maxv = (((uint32_t)1) << outbits) - 1;
+    while (inlen--) {
+        val = (val << inbits) | *(in++);
+        bits += inbits;
+        while (bits >= outbits) {
+            bits -= outbits;
+            out[(*outlen)++] = (val >> bits) & maxv;
+        }
+    }
+    if (pad) {
+        if (bits) {
+            out[(*outlen)++] = (val << (outbits - bits)) & maxv;
+        }
+    } else if (((val << (outbits - bits)) & maxv) || bits >= inbits) {
+        return 0;
+    }
+    return 1;
+}
+
+int segwit_addr_encode(char *output, const char *hrp, uint8_t witver, const uint8_t *witprog, size_t witprog_len) {
+    uint8_t data[65];
+    size_t datalen = 0;
+    if (witver > 16) return 0;
+    if (witver == 0 && witprog_len != 20 && witprog_len != 32) return 0;
+    if (witprog_len < 2 || witprog_len > 40) return 0;
+    data[0] = witver;
+    convert_bits(data + 1, &datalen, 5, witprog, witprog_len, 8, 1);
+    ++datalen;
+    return bech32_encode(output, hrp, data, datalen);
+}
+
+int segwit_addr_decode(int* witver, uint8_t* witdata, size_t* witdata_len, const char* hrp, const char* addr) {
+    uint8_t data[84];
+    char addr_lower[93];
+    size_t data_len;
+    size_t hrp_len;
+    size_t pos = 0;
+    int have_lower = 0;
+    int have_upper = 0;
+    while (pos < 93) {
+        char ch = addr[pos];
+        if (ch >= 'A' && ch <= 'Z') {
+            have_upper = 1;
+            ch = (ch - 'A') + 'a';
+        } else if (ch >= 'a' && ch <= 'z') {
+            have_lower = 1;
+        }
+        addr_lower[pos] = ch;
+        if (ch == 0) break;
+        ++pos;
+    }
+    if (pos == 93) return 0;
+    if (have_lower && have_upper) return 0;
+    if (!bech32_decode(&hrp_len, data, &data_len, addr_lower)) return 0;
+    if (data_len == 0 || data_len > 65) return 0;
+    if (strlen(hrp) != hrp_len) return 0;
+    if (memcmp(hrp, addr_lower, hrp_len) != 0) return 0;
+    if (data[0] > 16) return 0;
+    *witdata_len = 0;
+    if (!convert_bits(witdata, witdata_len, 8, data + 1, data_len - 1, 5, 0)) return 0;
+    if (*witdata_len < 2 || *witdata_len > 40) return 0;
+    if (data[0] == 0 && *witdata_len != 20 && *witdata_len != 32) return 0;
+    *witver = data[0];
+    return 1;
+}
---
+++ ref/c/tests.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "segwit_addr.h"
+
+static const char* valid_checksum[] = {
+    "a12uel5l",
+    "an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs",
+    "abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw",
+    "11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j",
+    "split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w",
+};
+
+struct valid_address_data {
+    const char* address;
+    size_t scriptPubKeyLen;
+    const uint8_t scriptPubKey[42];
+};
+
+static struct valid_address_data valid_address[] = {
+    {
+        "BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4",
+        22, {
+            0x00, 0x14, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
+            0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6
+        }
+    },
+    {
+        "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
+        34, {
+            0x00, 0x20, 0x18, 0x63, 0x14, 0x3c, 0x14, 0xc5, 0x16, 0x68, 0x04,
+            0xbd, 0x19, 0x20, 0x33, 0x56, 0xda, 0x13, 0x6c, 0x98, 0x56, 0x78,
+            0xcd, 0x4d, 0x27, 0xa1, 0xb8, 0xc6, 0x32, 0x96, 0x04, 0x90, 0x32,
+            0x62
+        }
+    },
+    {
+        "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx",
+        42, {
+            0x81, 0x28, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
+            0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6,
+            0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c,
+            0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6
+        }
+    },
+    {
+        "BC1SW50QA3JX3S",
+        4, {
+           0x90, 0x02, 0x75, 0x1e
+        }
+    },
+    {
+        "bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj",
+        18, {
+            0x82, 0x10, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
+            0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23
+        }
+    },
+    {
+        "tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
+        34, {
+            0x00, 0x20, 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62, 0x21,
+            0xb2, 0xa1, 0x87, 0x90, 0x5e, 0x52, 0x66, 0x36, 0x2b, 0x99, 0xd5,
+            0xe9, 0x1c, 0x6c, 0xe2, 0x4d, 0x16, 0x5d, 0xab, 0x93, 0xe8, 0x64,
+            0x33
+        }
+    }
+};
+
+static const char* invalid_address[] = {
+    "tc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty",
+    "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t5",
+    "BC13W508D6QEJXTDG4Y5R3ZARVARY0C5XW7KN40WF2",
+    "bc1rw5uspcuh",
+    "bc10w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90",
+    "BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
+    "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sI5k7",
+    "tb1pw508d6qejxtdg4y5r3zarqfsj6c3",
+    "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv",
+};
+
+static void segwit_scriptpubkey(uint8_t* scriptpubkey, size_t* scriptpubkeylen, int witver, const uint8_t* witprog, size_t witprog_len) {
+    scriptpubkey[0] = witver ? (0x80 | witver) : 0;
+    scriptpubkey[1] = witprog_len;
+    memcpy(scriptpubkey + 2, witprog, witprog_len);
+    *scriptpubkeylen = witprog_len + 2;
+}
+
+int main(void) {
+    size_t i;
+    int fail = 0;
+    for (i = 0; i < sizeof(valid_checksum) / sizeof(valid_checksum[0]); ++i) {
+        uint8_t data[82];
+        size_t data_len;
+        size_t hrp_len;
+        int ok = 1;
+        if (!bech32_decode(&hrp_len, data, &data_len, valid_checksum[i])) {
+            int32_t fault = bech32_decode_fault(&hrp_len, data, &data_len, valid_checksum[i]);
+            printf("bech32_decode fails: '%s': %i\n", valid_checksum[i], (int)fault);
+            ok = 0;
+        }
+        fail += !ok;
+    }
+    for (i = 0; i < sizeof(valid_address) / sizeof(valid_address[0]); ++i) {
+        uint8_t witprog[40];
+        size_t witprog_len;
+        int witver;
+        const char* hrp = "bc";
+        int ok = 1;
+        int ret = segwit_addr_decode(&witver, witprog, &witprog_len, hrp, valid_address[i].address);
+        if (!ret) {
+            hrp = "tb";
+            ret = segwit_addr_decode(&witver, witprog, &witprog_len, hrp, valid_address[i].address);
+        }
+        if (!ret) {
+            printf("segwit_addr_decode fails: '%s'\n", valid_address[i].address);
+            ok = 0;
+        }
+        uint8_t scriptpubkey[42];
+        size_t scriptpubkey_len;
+        if (ok) segwit_scriptpubkey(scriptpubkey, &scriptpubkey_len, witver, witprog, witprog_len);
+        if (ok && (scriptpubkey_len != valid_address[i].scriptPubKeyLen || memcmp(scriptpubkey, valid_address[i].scriptPubKey, scriptpubkey_len))) {
+            printf("segwit_addr_decode produces wrong result: '%s'\n", valid_address[i].address);
+            ok = 0;
+        }
+        char rebuild[93];
+        if (ok && !segwit_addr_encode(rebuild, hrp, witver, witprog, witprog_len)) {
+            printf("segwit_addr_encode fails: '%s'\n", valid_address[i].address);
+            ok = 0;
+        }
+        int p = 0;
+        while (ok && p < 93) {
+            int c = valid_address[i].address[p];
+            if (c >= 'A' && c <= 'Z') c = (c - 'A') + 'a';
+            if (c != rebuild[p]) {
+                printf("segwit_addr_encode produces wrong result: '%s'\n", valid_address[i].address);
+                ok = 0;
+            }
+            if (rebuild[p] == 0) break;
+            ++p;
+        }
+        fail += !ok;
+    }
+    for (i = 0; i < sizeof(invalid_address) / sizeof(invalid_address[0]); ++i) {
+        uint8_t witprog[40];
+        size_t witprog_len;
+        int witver;
+        int ok = 1;
+        if (segwit_addr_decode(&witver, witprog, &witprog_len, "bc", invalid_address[i])) {
+            printf("segwit_addr_decode succeeds on invalid address '%s'\n", invalid_address[i]);
+            ok = 0;
+        }
+        if (segwit_addr_decode(&witver, witprog, &witprog_len, "tb", invalid_address[i])) {
+            printf("segwit_addr_decode succeeds on invalid address '%s'\n", invalid_address[i]);
+            ok = 0;
+        }
+        fail += !ok;
+    }
+    printf("%i failures\n", fail);
+    return fail != 0;
+}
