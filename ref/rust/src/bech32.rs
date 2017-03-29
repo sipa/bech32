b5043135d64c65d330a1a0d269e96e699e143068
Clark <clarkmoody@gmail.com>
2017-03-29 12:02:38 -0500

    All tests passing
    Adds segwit address encoding and decoding
    
    Implements close method for Bech32 structure
    
    Adds mixed case checking for Bech32 data part
    
    Ignores Rust build artifacts
    
    Implements String encoding for Bech32 objects
    
    Adds Rust implementation: Bech32 decode working

---
+++ ref/rust/src/bech32.rs
@@ -0,0 +1,237 @@
+//! Encode and decode the Bech32 format, with checksums
+//! 
+//! 
+//! 
+
+use std::fmt;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Bech32 {
+    pub hrp: String,
+    pub data: Vec<u8>
+}
+
+impl Bech32 {
+    pub fn clone(&self) -> Bech32 {
+        Bech32 {
+            hrp: self.hrp.clone(),
+            data: self.data.to_vec()
+        }
+    }
+}
+
+// Human-readable part and data part separator
+const SEP: char = '1';
+
+// Encoding character set. Maps data value -> char
+const CHARSET: [char; 32] = [
+    'q','p','z','r','y','9','x','8',
+    'g','f','2','t','v','d','w','0',
+    's','3','j','n','5','4','k','h',
+    'c','e','6','m','u','a','7','l'
+];
+
+// Reverse character set. Maps ASCII byte -> CHARSET index on [0,31]
+const CHARSET_REV: [i8; 128] = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
+    -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+     1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
+    -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+     1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
+];
+
+// Generator coefficients
+const GEN: [u32; 5] = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+
+pub fn encode(b: Bech32) -> EncodeResult {
+    let hrp_bytes: Vec<u8> = b.hrp.clone().into_bytes();
+    let mut combined: Vec<u8> = b.data.clone();
+    combined.extend_from_slice(&create_checksum(&hrp_bytes, &b.data));
+    let mut ret: String = format!("{}{}", b.hrp, SEP);
+    for p in combined {
+        ret.push(CHARSET[p as usize]);
+    }
+    Ok(ret)
+}
+
+pub fn decode(s: String) -> DecodeResult {
+    // Ensure overall length is within bounds
+    let len: usize = s.len();
+    if len < 8 || len > 90 {
+        return Err(CodingError::InvalidLength)
+    }
+
+    // Split at separator and check for two pieces
+    let parts: Vec<&str> = s.rsplitn(2, SEP).collect();
+    if parts.len() != 2 {
+        return Err(CodingError::InvalidChar)
+    }
+    let raw_hrp = parts[1];
+    let raw_data = parts[0];
+    if raw_hrp.len() < 1 || raw_data.len() < 6 {
+        return Err(CodingError::InvalidLength)
+    }
+
+    let mut has_lower: bool = false;
+    let mut has_upper: bool = false;
+    let mut hrp_bytes: Vec<u8> = Vec::new();
+    for b in raw_hrp.bytes() {
+        // Valid subset of ASCII
+        if b < 33 || b > 126 {
+            return Err(CodingError::InvalidChar)
+        }
+        let mut c = b;
+        // Lowercase
+        if b >= 97 && b <= 122 {
+            has_lower = true;
+        }
+        // Uppercase
+        if b >= 65 && b <= 90 {
+            has_upper = true;
+            // Convert to lowercase
+            c = b + (97-65);
+        }
+        hrp_bytes.push(c);
+    }
+
+    // Ensure no mixed case
+    if has_lower && has_upper {
+        return Err(CodingError::MixedCase)
+    }
+
+    // Check data payload
+    let mut data_bytes: Vec<u8> = Vec::new();
+    for b in raw_data.bytes() {
+        // Aphanumeric only
+        if !((b >= 48 && b <= 57) || (b >= 65 && b <= 90) || (b >= 97 && b <= 122)) {
+            return Err(CodingError::InvalidChar)
+        }
+        // Excludes these characters: [1,b,i,o]
+        if b == 49 || b == 98 || b == 105 || b == 111 {
+            return Err(CodingError::InvalidChar)
+        }
+        // Lowercase
+        if b >= 97 && b <= 122 {
+            has_lower = true;
+        }
+        let mut c = b;
+        // Uppercase
+        if b >= 65 && b <= 90 {
+            has_upper = true;
+            // Convert to lowercase
+            c = b + (97-65);
+        }
+        data_bytes.push(CHARSET_REV[c as usize] as u8);
+    }
+
+    // Ensure no mixed case
+    if has_lower && has_upper {
+        return Err(CodingError::MixedCase)
+    }
+
+    // Ensure checksum
+    if !verify_checksum(&hrp_bytes, &data_bytes) {
+        return Err(CodingError::InvalidChecksum)
+    }
+
+    // Remove checksum from data payload
+    let dbl: usize = data_bytes.len();
+    data_bytes.truncate(dbl - 6);
+
+    Ok(Bech32 {
+        hrp: String::from_utf8(hrp_bytes).unwrap(),
+        data: data_bytes
+    })
+}
+
+fn create_checksum(hrp: &Vec<u8>, data: &Vec<u8>) -> Vec<u8> {
+    let mut values: Vec<u8> = hrp_expand(hrp);
+    values.extend_from_slice(data);
+    let pad: Vec<u8> = vec![0,0,0,0,0,0];
+    values.extend_from_slice(&pad);
+    let plm: u32 = polymod(values) ^ 1;
+    let mut checksum: Vec<u8> = Vec::new();
+    for p in 0..6 {
+        checksum.push(((plm >> 5 * (5 - p)) & 0x1f) as u8);
+    }
+    checksum
+}
+
+fn verify_checksum(hrp: &Vec<u8>, data: &Vec<u8>) -> bool {
+    let mut exp = hrp_expand(hrp);
+    exp.extend_from_slice(data);
+    polymod(exp) == 1u32
+}
+
+fn hrp_expand(hrp: &Vec<u8>) -> Vec<u8> {
+    let mut v: Vec<u8> = Vec::new();
+    for b in hrp {
+        v.push(*b >> 5);
+    }
+    v.push(0);
+    for b in hrp {
+        v.push(*b & 0x1f);
+    }
+    v
+}
+
+fn polymod(values: Vec<u8>) -> u32 {
+    let mut chk: u32 = 1;
+    let mut b: u8;
+    for v in values {
+        b = (chk >> 25) as u8;
+        chk = (chk & 0x1ffffff) << 5 ^ (v as u32);
+        for i in 0..5 {
+            if (b >> i) & 1 == 1 {
+                chk ^= GEN[i]
+            }
+        }
+    }
+    chk
+}
+
+#[derive(Debug)]
+pub enum CodingError {
+    InvalidChecksum,
+    InvalidLength,
+    InvalidChar,
+    MixedCase,
+}
+
+impl fmt::Display for CodingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CodingError::InvalidChecksum => write!(f, "Invalid checksum."),
+            CodingError::InvalidLength => write!(f, "Invalid length."),
+            CodingError::InvalidChar => write!(f, "Invalid character."),
+            CodingError::MixedCase => write!(f, "Mixed-case strings not allowed."),
+        }
+    }
+}
+
+impl Error for CodingError {
+    fn description(&self) -> &str {
+        match *self {
+            CodingError::InvalidChecksum => "Invalid checksum.",
+            CodingError::InvalidLength => "Invalid length.",
+            CodingError::InvalidChar => "Invalid character.",
+            CodingError::MixedCase => "Mixed-case strings not allowed.",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            CodingError::InvalidChecksum => None,
+            CodingError::InvalidLength => None,
+            CodingError::InvalidChar => None,
+            CodingError::MixedCase => None,
+        }
+    }
+}
+
+pub type DecodeResult = Result<Bech32, CodingError>;
+pub type EncodeResult = Result<String, CodingError>;
---
+++ ref/rust/src/lib.rs
@@ -0,0 +1,135 @@
+#![warn(missing_docs)]
+
+pub mod bech32;
+pub mod segwit_addr;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_checksum() {
+        let strings: Vec<&str> = vec!(
+            "A12UEL5L",
+            "an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs",
+            "abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw",
+            "11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j",
+            "split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w",
+        );
+        for s in strings {
+            let decode_result = bech32::decode(s.to_string());
+            if !decode_result.is_ok() {
+                println!("Did not decode: {:?}", s);
+            }
+            assert!(decode_result.is_ok());
+            let encode_result = bech32::encode(decode_result.unwrap());
+            assert!(encode_result.is_ok());
+            assert_eq!(s.to_lowercase(), encode_result.unwrap().to_lowercase());
+        }
+    }
+
+    #[test]
+    fn valid_address() {
+        let pairs: Vec<(&str, Vec<u8>)> = vec![
+            (
+                "BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4",
+                vec![
+                    0x00, 0x14, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
+                    0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6
+                ]
+            ),
+            (
+                "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
+                vec![
+                    0x00, 0x20, 0x18, 0x63, 0x14, 0x3c, 0x14, 0xc5, 0x16, 0x68, 0x04,
+                    0xbd, 0x19, 0x20, 0x33, 0x56, 0xda, 0x13, 0x6c, 0x98, 0x56, 0x78,
+                    0xcd, 0x4d, 0x27, 0xa1, 0xb8, 0xc6, 0x32, 0x96, 0x04, 0x90, 0x32,
+                    0x62
+                ]
+            ),
+            (
+                "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx",
+                vec![
+                    0x81, 0x28, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
+                    0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6,
+                    0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c,
+                    0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6
+                ]
+            ),
+            (
+                "BC1SW50QA3JX3S",
+                vec![
+                   0x90, 0x02, 0x75, 0x1e
+                ]
+            ),
+            (
+                "bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj",
+                vec![
+                    0x82, 0x10, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
+                    0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23
+                ]
+            ),
+            (
+                "tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
+                vec![
+                    0x00, 0x20, 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62, 0x21,
+                    0xb2, 0xa1, 0x87, 0x90, 0x5e, 0x52, 0x66, 0x36, 0x2b, 0x99, 0xd5,
+                    0xe9, 0x1c, 0x6c, 0xe2, 0x4d, 0x16, 0x5d, 0xab, 0x93, 0xe8, 0x64,
+                    0x33
+                ]
+            ),
+        ];
+        for p in pairs {
+            let (address, scriptpubkey) = p;
+            let mut hrp = "bc".to_string();
+            let mut dec_result = segwit_addr::decode(hrp.clone(), 
+                address.to_string());
+            if !dec_result.is_ok() {
+                hrp = "tb".to_string();
+                dec_result = segwit_addr::decode(hrp.clone(), 
+                    address.to_string());
+                if !dec_result.is_ok() {
+                    println!("Should be valid: {:?}", address);
+                }
+            }
+            assert!(dec_result.is_ok());
+
+            let prog = dec_result.unwrap();
+            let pubkey = prog.clone().to_scriptpubkey();
+            assert_eq!(pubkey, scriptpubkey);
+
+            let enc_result = segwit_addr::encode(hrp, prog);
+            assert!(enc_result.is_ok());
+
+            let enc_address = enc_result.unwrap();
+            assert_eq!(address.to_lowercase(), enc_address.to_lowercase());
+        }
+    }
+
+    #[test]
+    fn invalid_address() {
+        let strings: Vec<&str> = vec!(
+            "tc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty",
+            "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t5",
+            "BC13W508D6QEJXTDG4Y5R3ZARVARY0C5XW7KN40WF2",
+            "bc1rw5uspcuh",
+            "bc10w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90",
+            "BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
+            "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sL5k7",
+            "tb1pw508d6qejxtdg4y5r3zarqfsj6c3",
+            "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv",
+        );
+        for s in strings {
+            let bc_dec_result = segwit_addr::decode("bc".to_string(), s.to_string());
+            let tb_dec_result = segwit_addr::decode("tb".to_string(), s.to_string());
+            if bc_dec_result.is_ok() {
+                println!("Should be invalid: {:?}", s);
+            }
+            if tb_dec_result.is_ok() {
+                println!("Should be invalid: {:?}", s);
+            }
+            assert!(bc_dec_result.is_err());
+            assert!(tb_dec_result.is_err());
+        }
+    }
+}
