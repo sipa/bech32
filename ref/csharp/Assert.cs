159d859449c50071faf662e324dd03dd811499c4
NicolasDorier <nicolas.dorier@gmail.com>
2017-06-23 18:44:47 +0900

    Add C# reference implementation

---
+++ ref/csharp/Assert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp
+{
+    public static class Assert
+    {
+        public static void Equal<T>(T a, T b)
+        {
+            if (!a.Equals(b))
+                throw new Exception("a != b");
+        }
+
+        public static T Throws<T>(Action act) where T : Exception
+        {
+            try
+            {
+                act();
+            }
+            catch (T ex)
+            {
+                return ex;
+            }
+            throw new Exception("Should have thrown");
+        }
+    }
+}
---
+++ ref/csharp/README.md
@@ -0,0 +1,16 @@
+# Bech32 C#
+
+Implement Bech32 with error detection in C#.
+Build the code using dotnet core:
+```
+$ dotnet restore
+$ dotnet build
+```
+
+Run the tests using:
+```
+$ dotnet run
+```
+
+This is a reference implementation not meant to be used as library.
+Bech32 is implemented as a part of [NBitcoin library](https://github.com/MetacoSA/NBitcoin).
