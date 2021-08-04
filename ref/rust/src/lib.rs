9881d73466296a10bd5a3cc6fbcee23aedec3d09
Andrew Poelstra <apoelstra@wpsoftware.net>
2021-08-04 17:36:28 +0000

    update rust reference code for bech32m
    Also add support for "bcrt" HRP

--- ref/rust/src/lib.rs
+++ ref/rust/src/lib.rs
@@ -21,34 +21,34 @@
 #![warn(missing_docs)]
 
 //! Encoding and decoding Bech32 Bitcoin Segwit Addresses
-//! 
-//! Encoding and decoding for Bech32 strings and Bitcoin Segregated Witness 
+//!
+//! Encoding and decoding for Bech32 strings and Bitcoin Segregated Witness
 //! addresses. Bech32 is a 5-bit (base-32) encoding scheme that produces
 //! strings that comprise a human-readable part, a separator, a data part,
 //! and a checksum. The encoding implements a BCH code that guarantees
 //! error detection of up to four characters with less than 1 in 1 billion
 //! chance of failing to detect more errors.
-//! 
+//!
 //! The library contains `bech32` utilities for generic encoding of Bech32
 //! strings and `wit_prog` for converting witness programs to Bitcoin
 //! addresses and back.
-//! 
+//!
 //! # Examples
-//! 
+//!
 //! ```rust
 //! use bech32::wit_prog::WitnessProgram;
-//! 
+//!
 //! let witness_program = WitnessProgram {
 //!     version: 0,
 //!     program: vec![
-//!                 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62, 
-//!                 0x21, 0xb2, 0xa1, 0x87, 0x90, 0x5e, 0x52, 0x66, 
-//!                 0x36, 0x2b, 0x99, 0xd5, 0xe9, 0x1c, 0x6c, 0xe2, 
+//!                 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62,
+//!                 0x21, 0xb2, 0xa1, 0x87, 0x90, 0x5e, 0x52, 0x66,
+//!                 0x36, 0x2b, 0x99, 0xd5, 0xe9, 0x1c, 0x6c, 0xe2,
 //!                 0x4d, 0x16, 0x5d, 0xab, 0x93, 0xe8, 0x64, 0x33]
 //! };
-//! 
+//!
 //! let enc_result = witness_program.to_address("tb".to_string());
-//! assert_eq!(enc_result.unwrap(), 
+//! assert_eq!(enc_result.unwrap(),
 //!     "tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy".to_string());
 //! ```
 
@@ -97,6 +97,11 @@ pub enum WitnessProgramError {
     InvalidVersionLength,
     /// Script version must be 0 to 16 inclusive
     InvalidScriptVersion,
+    /// Improper encoding used for address
+    ///
+    /// Witness version 0 addresses must use Bech32 encoding, and all other
+    /// versions must use Bech32m
+    InvalidEncoding,
 }
 
 /// Error types during bit conversion
@@ -121,6 +126,27 @@ pub enum AddressError {
     HumanReadableMismatch,
     /// The human-readable part is invalid (must be "bc" or "tb")
     InvalidHumanReadablePart,
+}
+
+#[doc(hidden)]
+impl From<CodingError> for AddressError {
+    fn from(e: CodingError) -> Self {
+        AddressError::Bech32(e)
+    }
+}
+
+#[doc(hidden)]
+impl From<WitnessProgramError> for AddressError {
+    fn from(e: WitnessProgramError) -> Self {
+        AddressError::WitnessProgram(e)
+    }
+}
+
+#[doc(hidden)]
+impl From<BitConversionError> for AddressError {
+    fn from(e: BitConversionError) -> Self {
+        AddressError::Conversion(e)
+    }
 }
 
 #[cfg(test)]
@@ -182,67 +208,89 @@ mod tests {
 
     #[test]
     fn valid_address() {
-        let pairs: Vec<(&str, Vec<u8>)> = vec![
+        let pairs: Vec<(&str, Vec<u8>, &'static str)> = vec![
             (
                 "BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4",
                 vec![
-                    0x00, 0x14, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
-                    0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6
-                ]
+                    0x00, 0x14, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c,
+                    0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6,
+                ],
+                "bc",
             ),
             (
                 "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
                 vec![
-                    0x00, 0x20, 0x18, 0x63, 0x14, 0x3c, 0x14, 0xc5, 0x16, 0x68, 0x04,
-                    0xbd, 0x19, 0x20, 0x33, 0x56, 0xda, 0x13, 0x6c, 0x98, 0x56, 0x78,
-                    0xcd, 0x4d, 0x27, 0xa1, 0xb8, 0xc6, 0x32, 0x96, 0x04, 0x90, 0x32,
-                    0x62
-                ]
+                    0x00, 0x20, 0x18, 0x63, 0x14, 0x3c, 0x14, 0xc5, 0x16, 0x68, 0x04, 0xbd, 0x19,
+                    0x20, 0x33, 0x56, 0xda, 0x13, 0x6c, 0x98, 0x56, 0x78, 0xcd, 0x4d, 0x27, 0xa1,
+                    0xb8, 0xc6, 0x32, 0x96, 0x04, 0x90, 0x32, 0x62,
+                ],
+                "tb",
             ),
             (
-                "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx",
+                "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y",
                 vec![
-                    0x51, 0x28, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
-                    0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6,
-                    0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c,
-                    0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6
-                ]
+                    0x51, 0x28, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c,
+                    0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6, 0x75, 0x1e, 0x76, 0xe8,
+                    0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1,
+                    0x43, 0x3b, 0xd6,
+                ],
+                "bc",
             ),
             (
-                "BC1SW50QA3JX3S",
-                vec![
-                   0x60, 0x02, 0x75, 0x1e
-                ]
+                "BC1SW50QGDZ25J",
+                vec![0x60, 0x02, 0x75, 0x1e],
+                "bc",
             ),
             (
-                "bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj",
+                "bc1zw508d6qejxtdg4y5r3zarvaryvaxxpcs",
                 vec![
-                    0x52, 0x10, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54,
-                    0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23
-                ]
+                    0x52, 0x10, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c,
+                    0x45, 0xd1, 0xb3, 0xa3, 0x23,
+                ],
+                "bc",
             ),
             (
                 "tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
                 vec![
-                    0x00, 0x20, 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62, 0x21,
-                    0xb2, 0xa1, 0x87, 0x90, 0x5e, 0x52, 0x66, 0x36, 0x2b, 0x99, 0xd5,
-                    0xe9, 0x1c, 0x6c, 0xe2, 0x4d, 0x16, 0x5d, 0xab, 0x93, 0xe8, 0x64,
-                    0x33
-                ]
+                    0x00, 0x20, 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62, 0x21, 0xb2, 0xa1,
+                    0x87, 0x90, 0x5e, 0x52, 0x66, 0x36, 0x2b, 0x99, 0xd5, 0xe9, 0x1c, 0x6c, 0xe2,
+                    0x4d, 0x16, 0x5d, 0xab, 0x93, 0xe8, 0x64, 0x33,
+                ],
+                "tb",
+            ),
+            (
+                "bcrt1qn3h68k2u0rr49skx05qw7veynpf4lfppd2demt",
+                vec![
+                    0x00, 0x14, 0x9c, 0x6f, 0xa3, 0xd9, 0x5c, 0x78, 0xc7, 0x52, 0xc2, 0xc6, 0x7d,
+                    0x00, 0xef, 0x33, 0x24, 0x98, 0x53, 0x5f, 0xa4, 0x21,
+                ],
+                "bcrt",
+            ),
+            (
+                "tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c",
+                vec![
+                    0x51, 0x20, 0x00, 0x00, 0x00, 0xc4, 0xa5, 0xca, 0xd4, 0x62, 0x21, 0xb2, 0xa1,
+                    0x87, 0x90, 0x5e, 0x52, 0x66, 0x36, 0x2b, 0x99, 0xd5, 0xe9, 0x1c, 0x6c, 0xe2,
+                    0x4d, 0x16, 0x5d, 0xab, 0x93, 0xe8, 0x64, 0x33,
+                ],
+                "tb",
+            ),
+            (
+                "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0",
+                vec![
+                    0x51, 0x20, 0x79, 0xbe, 0x66, 0x7e, 0xf9, 0xdc, 0xbb, 0xac, 0x55, 0xa0, 0x62,
+                    0x95, 0xce, 0x87, 0x0b, 0x07, 0x02, 0x9b, 0xfc, 0xdb, 0x2d, 0xce, 0x28, 0xd9,
+                    0x59, 0xf2, 0x81, 0x5b, 0x16, 0xf8, 0x17, 0x98,
+                ],
+                "bc",
             ),
         ];
         for p in pairs {
-            let (address, scriptpubkey) = p;
-            let mut hrp = "bc".to_string();
-            let mut dec_result = wit_prog::WitnessProgram::from_address(hrp.clone(),
+            let (address, scriptpubkey, hrp) = p;
+            let dec_result = wit_prog::WitnessProgram::from_address(hrp.to_string(),
                 address.to_string());
             if !dec_result.is_ok() {
-                hrp = "tb".to_string();
-                dec_result = wit_prog::WitnessProgram::from_address(hrp.clone(),
-                    address.to_string());
-                if !dec_result.is_ok() {
-                    println!("Should be valid: {:?}", address);
-                }
+                println!("Should be valid: {:?}", address);
             }
             assert!(dec_result.is_ok());
 
@@ -254,7 +302,7 @@ mod tests {
             assert!(spk_result.is_ok());
             assert_eq!(prog, spk_result.unwrap());
 
-            let enc_result = prog.to_address(hrp);
+            let enc_result = prog.to_address(hrp.to_string());
             assert!(enc_result.is_ok());
 
             let enc_address = enc_result.unwrap();
@@ -265,24 +313,95 @@ mod tests {
     #[test]
     fn invalid_address() {
         let pairs: Vec<(&str, AddressError)> = vec!(
-            ("tc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty",
-                AddressError::InvalidHumanReadablePart),
-            ("bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t5",
-                AddressError::Bech32(CodingError::InvalidChecksum)),
-            ("BC13W508D6QEJXTDG4Y5R3ZARVARY0C5XW7KN40WF2",
-                AddressError::WitnessProgram(WitnessProgramError::InvalidScriptVersion)),
-            ("bc1rw5uspcuh",
-                AddressError::WitnessProgram(WitnessProgramError::InvalidLength)),
-            ("bc10w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90",
-                AddressError::Bech32(CodingError::InvalidLength)),
-            ("BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
-                AddressError::WitnessProgram(WitnessProgramError::InvalidVersionLength)),
-            ("tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sL5k7",
-                AddressError::Bech32(CodingError::MixedCase)),
-            ("tb1pw508d6qejxtdg4y5r3zarqfsj6c3",
-                AddressError::Conversion(BitConversionError::InvalidPadding)),
-            ("tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv",
-                AddressError::Conversion(BitConversionError::InvalidPadding)),
+            // BIP-0173 Invalid Addresses
+            (
+                "tc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty",
+                AddressError::InvalidHumanReadablePart,
+            ),
+            (
+                "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t5",
+                CodingError::InvalidChecksum.into(),
+            ),
+            (
+                "BC13W508D6QEJXTDG4Y5R3ZARVARY0C5XW7KN40WF2",
+                WitnessProgramError::InvalidScriptVersion.into(),
+            ),
+            ("bc1rw5uspcuh", WitnessProgramError::InvalidLength.into()),
+            (
+                "bc10w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90",
+                CodingError::InvalidLength.into(),
+            ),
+            (
+                "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sL5k7",
+                CodingError::MixedCase.into(),
+            ),
+            (
+                "tb1pw508d6qejxtdg4y5r3zarqfsj6c3",
+                BitConversionError::InvalidPadding.into(),
+            ),
+            (
+                "bc1zw508d6qejxtdg4y5r3zarvaryvqyzf3du",
+                BitConversionError::InvalidPadding.into(),
+            ),
+            (
+                "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3pjxtptv",
+                BitConversionError::InvalidPadding.into(),
+            ),
+            // BIP-0350 Invalid Addresses
+            (
+                "tc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq5zuyut",
+                AddressError::InvalidHumanReadablePart,
+            ),
+            (
+                "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqh2y7hd",
+                WitnessProgramError::InvalidEncoding.into(),
+            ),
+            (
+                "tb1z0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqglt7rf",
+                WitnessProgramError::InvalidEncoding.into(),
+            ),
+            (
+                "BC1S0XLXVLHEMJA6C4DQV22UAPCTQUPFHLXM9H8Z3K2E72Q4K9HCZ7VQ54WELL",
+                WitnessProgramError::InvalidEncoding.into(),
+            ),
+            (
+                "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kemeawh",
+                WitnessProgramError::InvalidEncoding.into(),
+            ),
+            (
+                "tb1q0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq24jc47",
+                WitnessProgramError::InvalidEncoding.into(),
+            ),
+            (
+                "bc1p38j9r5y49hruaue7wxjce0updqjuyyx0kh56v8s25huc6995vvpql3jow4",
+                CodingError::InvalidChar.into(),
+            ),
+            (
+                "BC130XLXVLHEMJA6C4DQV22UAPCTQUPFHLXM9H8Z3K2E72Q4K9HCZ7VQ7ZWS8R",
+                WitnessProgramError::InvalidScriptVersion.into(),
+            ),
+            ("bc1pw5dgrnzv", WitnessProgramError::InvalidLength.into()),
+            (
+                "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7v8n0nx0muaewav253zgeav",
+                CodingError::InvalidLength.into(),
+            ),
+            (
+                "BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
+                WitnessProgramError::InvalidVersionLength.into(),
+            ),
+            (
+                "tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq47Zagq",
+                CodingError::MixedCase.into(),
+            ),
+            (
+                "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7v07qwwzcrf",
+                BitConversionError::InvalidPadding.into(),
+            ),
+            (
+                "tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vpggkg4j",
+                BitConversionError::InvalidPadding.into(),
+            ),
+            ("bc1gmk9yu", CodingError::InvalidLength.into()),
         );
         for p in pairs {
             let (address, desired_error) = p;
